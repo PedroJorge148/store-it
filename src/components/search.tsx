28235a9acbadd62af57c65ae21f0256e4cb9d63a
Pedro Jorge <pedrojorge2011@gmail.com>
2025-03-03 10:06:16 -0300

    finish search component with debounce

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -3,36 +3,57 @@
 import Image from 'next/image'
 import { Input } from './ui/input'
 import { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { getFiles } from '@/lib/actions/file.actions'
 import { Models } from 'node-appwrite'
 import { Thumbnail } from './thumbnail'
 import { FormattedDateTime } from './formatted-date-time'
+import { useDebounce } from '@/hooks/use-debounce'
 
 export function Search() {
   const [query, setQuery] = useState('')
   const [open, setOpen] = useState(false)
   const [results, setResults] = useState<Models.Document[]>([])
 
+  const path = usePathname()
+  const router = useRouter()
   const searchParams = useSearchParams()
   const searchQuery = searchParams.get('query') || ''
 
+  const debouncedQuery = useDebounce(query, 300)
+
   useEffect(() => {
     async function fetchFiles() {
-      // TODO remove types?
-      const files = await getFiles({ types: [], searchText: query })
+      if (!debouncedQuery) {
+        console.log('here')
+        setResults([])
+        setOpen(false)
+        return router.push(path.replace(searchParams.toString(), ''))
+      }
+
+      const files = await getFiles({ types: [], searchText: debouncedQuery })
 
       setResults(files.documents)
       setOpen(true)
     }
     fetchFiles()
-  }, [query])
+  }, [debouncedQuery])
 
   useEffect(() => {
-    if (searchQuery) {
+    if (!searchQuery) {
       setQuery('')
     }
   }, [searchQuery])
+
+  function handleClickItem(file: Models.Document) {
+    setOpen(false)
+    setResults([])
+
+    router.push(`/${file.type === 'video' || file.type === 'audio'
+? 'media'
+: file.type + 's'}?query=${debouncedQuery}`,
+    )
+  }
 
   return (
     <div className="search">
@@ -58,6 +79,7 @@ export function Search() {
                     <li
                       key={file.$id}
                       className="flex items-center justify-between"
+                      onClick={() => handleClickItem(file)}
                     >
                       <div className="flex cursor-pointer items-center gap-4">
                         <Thumbnail
