cd7c5369f27661c1ca84c02f3cc9f485c14e25d7
Pedro Jorge <pedrojorge2011@gmail.com>
2025-03-03 10:18:19 -0300

    add sort component

--- src/components/sort.tsx
+++ src/components/sort.tsx
@@ -1,5 +1,36 @@
+'use client'
+
+import { usePathname, useRouter } from 'next/navigation'
+import {
+  Select, SelectContent,
+  SelectItem, SelectTrigger, SelectValue,
+} from './ui/select'
+import { sortTypes } from '@/constants'
+
 export function Sort() {
+  const path = usePathname()
+  const router = useRouter()
+
+  function handleSort(value: string) {
+    router.push(`${path}?sort=${value}`)
+  }
+
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.value}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   )
 }
